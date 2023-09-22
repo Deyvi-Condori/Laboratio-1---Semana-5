b719f75c69423c66d58c3e68de5c3d528e2649cf
Deyvi Condori <deivcondor@gmail.com>
2023-09-21 22:58:03 -0500

    Agregar archivos de proyecto.

---
+++ Laboratio 1- Semana 5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MultiSinMulti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el numero 1: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el numero 2: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            int resultado = 0;
+
+            int limite = numero2;
+
+            if (numero2 < 0)
+            {
+                limite = -numero2;
+            }
+
+            for (int i = 0; i < limite; i++)
+            {
+                resultado += numero1;
+            }
+
+            if ((numero1 < 0) ^ (numero2 < 0)) 
+            {
+                resultado = -resultado;
+            }
+
+            Console.WriteLine($"El producto es {resultado}");
+            Console.ReadKey();
+        }
+    }
+}
